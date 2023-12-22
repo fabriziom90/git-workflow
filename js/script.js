935c4f40b25638a7156280c33b91a8a45d65cd74
fabriziom90 <fabriziomastrobattista@gmail.com>
2023-12-22 12:05:19 +0100

    [UPDATE] Added logic for toggle button and styles

--- js/script.js
+++ js/script.js
@@ -5,12 +5,21 @@ createApp({
         return {
             message: "Esempio di git workflow",
             count: 0,
-            text: ''
+            text: '',
+            color: 'red'
         }
     },
     methods: {
         countClick() {
             this.count++;
+        },
+        toggleColor() {
+            if (this.color == 'red') {
+                this.color = 'blue'
+            }
+            else {
+                this.color = 'red'
+            }
         }
     }
 
