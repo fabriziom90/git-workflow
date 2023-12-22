b17de4fc9c46f6d022d3d7885f672d28bd97e8de
fabriziom90 <fabriziomastrobattista@gmail.com>
2023-12-22 11:37:13 +0100

    [UPDATE] Added logic of counter

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Esempio di git workflow"
+            message: "Esempio di git workflow",
+            click: 0
         }
     },
     methods: {
-
+        countClick() {
+            this.count++;
+        }
     }
 
 }).mount('#app');
